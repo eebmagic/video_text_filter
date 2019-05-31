5b9acc623a028c1f42b65333d6afb204736ecda1
Ethan B <eebmagic@gmail.com>
2019-05-31 00:21:34 -0500

    Added func for flattening list & getting image/terminal window sizes

--- video.py
+++ video.py
@@ -1,21 +1,48 @@
 import numpy as np
 import cv2
+import os
+
+def flattenList(inputList):
+    output = inputList[:]
+    while type(output[0]) == list:
+        temp = []
+        for sub in output:
+            temp += sub
+        output = temp[:]            
+    return output
+
+
+
 
 cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    print(len(frame), len(frame[0]))
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    print(len(gray))
+    
+    # Get image size and window size
+    org_height = len(gray.tolist())
+    org_width = len(gray.tolist()[0])
+    screen_height,  screen_width = os.popen('stty size', 'r').read().split()
+    
+    
+    print(screen_width, screen_height)
+    print(org_width, org_height)
+
 
     # Display the resulting frame
     cv2.imshow('frame',gray)
 
+    #0-255
+    flatGray = flattenList(gray.tolist())
 
+
+
+    print("")
+    #Test for break
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
